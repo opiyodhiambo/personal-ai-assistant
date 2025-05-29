cfbee38a201c9781a852cb5ac07efdffea5ddb6c
opiyodhiambo <rnldlinus@gmail.com>
2025-05-29 12:52:40 +0300

    make our chat application memory aware

--- apps/chat/views.py
+++ apps/chat/views.py
@@ -6,7 +6,6 @@ from rest_framework.views import APIView
 from rest_framework import status
 from apps.chat.clients.whatsapp_client import WhatsAppClient
 from apps.chat.config import WhatsAppConfig
-from apps.llm.clients.ollama_client import OllamaClient
 import logging
 
 logging.basicConfig(
@@ -19,41 +18,12 @@ class WhatsAppWebhookView(APIView):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.whatsapp = WhatsAppClient()
-        self.ollama = OllamaClient()
         self.config = WhatsAppConfig()
 
     
     def post(self, request):
-        try:
-
-            # We prepare our prompt
-            message = request.data['entry'][0]['changes'][0]['value']['messages'][0]
-            sender = message['from']
-            timestamp = message['timestamp']
-            prompt = message['text']['body']
-
-            # ask our LLM
-            start_time = datetime.now()
-            print(f"start_time {start_time}")
-
-            full_reply = self.ollama.ask(prompt)
-            end_time = datetime.now()
-            print(f"\n{full_reply}\nPrompt ended at {end_time}")
-
-            duration = (end_time - start_time).total_seconds()
-            print(f"This prompt took {duration} seconds")
-            
-            self.whatsapp.send_message(to=sender, text=full_reply)
-            return Response({"status": "sent"}, status=status.HTTP_200_OK)
-        except Exception as e:
-            return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+        pass
 
 
     def get(self, request):
-        mode = request.GET.get('hub.mode')
-        token = request.GET.get('hub.verify_token')
-        challenge = request.GET.get('hub.challenge')
-
-        if mode == 'subscribe' and token == self.config.verify_token:
-            return Response({'hub.challenge': challenge})
-        return Response({}, status=status.HTTP_403_FORBIDDEN)
+        pass
--- apps/llm/query_pipeline.py
+++ apps/llm/query_pipeline.py
@@ -1,32 +1,49 @@
+from langchain_ollama import ChatOllama
 from apps.llm.clients.google_calendar import GoogleCalendar
-from apps.llm.clients.ollama_client import OllamaClient
 from apps.llm.embedding.embedder import EmbeddingModel
 from apps.llm.models import UserIntent
 from apps.llm.prompt_engineering.prompt_optimizer import PromptOptimizer
 from apps.llm.service.intents import IntentDetector
+from langchain.memory import ConversationSummaryMemory
+from langchain.chains.conversation.base import ConversationChain
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.runnables.history import RunnableWithMessageHistory
 
 class QueryPipeline:
     def __init__(self):
         self.embedder = EmbeddingModel()
         self.prompt_optimizer = PromptOptimizer()
-        self.llm = OllamaClient()
         self.intent_detector = IntentDetector()
         self.calendar = GoogleCalendar()
+        
+        # For a memory-aware chat
+        self.memory_store = {}
+        self.chat_llm = ChatOllama(model="llama3.2")
+        self.prompt = ChatPromptTemplate.from_messages([
+            ("system", "You are a personal assistant"),
+            MessagesPlaceholder(variable_name="history"),
+            ("human", "{input}")
+        ])
+        self.memory_chain = RunnableWithMessageHistory(
+            self.prompt | self.chat_llm,
+            get_session_history=self._get_memory,
+            input_messages_key="input",
+            history_messages_key="history"
 
-    def run(self, user_query):
+        )
+
+
+    async def run(self, user_query: str):
         response = ""
         
         # Checking user intent
         user_intent = self.intent_detector.classify(user_query)
         
         if user_intent == UserIntent.CREATE_EVENT:
-            event_data = self.intent_detector.extract_event(user_query)
-            self.calendar.add_event(event_data)
-            response += self.call_llm(user_query=user_query, context=event_data, user_intent=user_intent)
+            return await self._handle_create_event(user_query=user_query)
 
         elif user_intent == UserIntent.QUERY_CALENDAR:
-            upcoming_events = self.calendar.get_upcoming_events()
-            response += self.call_llm(user_query=user_query, context=upcoming_events, user_intent=user_intent)
+            return await self._handle_calendar_query(user_query=user_query)
 
         elif user_intent == UserIntent.GENERAL:
             context = self.embedder.retrieve_context(user_query)
@@ -35,9 +52,48 @@ class QueryPipeline:
         return response
     
 
-    def call_llm(self, user_query, context, user_intent) -> str:
+    async def _handle_create_event(self, user_query: str, stream: bool=False):
+        event_data = self.intent_detector.extract_event(user_query)
+        if not event_data.get("start_time"):
+            return "⚠️ I couldn't understand when to schedule the event. Please specify a clear time."
+
+        self.calendar.add_event(event_data)
+        return await self.call_llm(user_query, context=event_data, user_intent=UserIntent.CREATE_EVENT, stream=stream)
+    
+
+    async def _handle_calendar_query(self, user_query: str, stream: bool=False):
+        events = self.calendar.get_upcoming_events()
+        return await self.call_llm(user_query, context=events, user_intent=UserIntent.QUERY_CALENDAR, stream=stream)
+    
+
+    async def _handle_general_query(self, user_query: str, stream: bool=False):
+        context = self.embedder.retrieve_context(user_query)
+        return await self.call_llm(user_query, context=context, user_intent=UserIntent.GENERAL, stream=stream)
+
+
+    async def call_llm(self, user_query: str, context: str, session_id: str, user_intent: UserIntent, stream: bool):
         optimized_prompt = self.prompt_optimizer.build(user_query, context, user_intent)
-        return self.llm.ask(optimized_prompt)
+
+        if stream:
+            async for chunk in self.memory_chain.astream(
+                {"input": optimized_prompt},
+                config={"configurable": {"session_id": session_id}}
+            ):
+                yield chunk
+        else:
+            return await self.memory_chain.ainvoke(
+                {"input": optimized_prompt},
+                config={"configurable": {"session_id": session_id}}
+            )
+    
+
+    def _get_memory(self, session_id: str):
+        if session_id not in self.memory_store:
+            self.memory_store[session_id] = ConversationSummaryMemory(
+                llm=self.chat_llm,
+                memory_key="history"
+            )
+        return self.memory_store[session_id]
 
     
 
